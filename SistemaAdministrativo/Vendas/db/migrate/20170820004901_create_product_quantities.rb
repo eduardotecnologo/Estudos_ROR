9baaa914a0bffd1ea7ca90274ccd9683d0f3a8c5
eduardodeveloper <eduardotecnologo@hotmail.com>
2017-08-20 00:01:35 -0300

    Admin Isis

---
+++ SistemaAdministrativo/Vendas/db/migrate/20170820004901_create_product_quantities.rb
@@ -0,0 +1,12 @@
+class CreateProductQuantities < ActiveRecord::Migration[5.0]
+  def change
+    create_table :product_quantities do |t|
+      t.references :product, foreign_key: true
+      t.integer :quantity
+      t.references :user, foreign_key: true
+      t.text :notes
+
+      t.timestamps
+    end
+  end
+end
